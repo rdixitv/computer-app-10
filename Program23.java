267e525100ba50dccb60ff7f694faaffb4c8ce5c
RD <raghavd@disroot.org>
2024-02-14 17:28:11 +0530

    Fixed Prog23

--- Program23.java
+++ Program23.java
@@ -7,17 +7,17 @@ import java.util.Scanner;
 public class Program23 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        char arr[][] = new char[3][3];
+        int arr[][] = new int[3][3];
 
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++) {
-                System.out.print("Enter a character: ");
-                arr[i][j] = sc.next().charAt(0);
+                System.out.print("Enter a number: ");
+                arr[i][j] = sc.nextInt();
             }
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++)
-                System.out.print((arr[i][j] % 2 == 0 ? arr[i][j] : '*') + "\t");
+                System.out.print((arr[i][j] % 2 == 0 ? arr[i][j] : "*") + "\t");
             System.out.println();
         }
     }
