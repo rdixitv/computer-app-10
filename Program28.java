f41757de08a04358340d700b5ac40654345aa116
RD <raghavd@disroot.org>
2024-02-19 21:11:10 +0530

    Fixed mistake

--- Program28.java
+++ Program28.java
@@ -26,7 +26,7 @@ public class Program28 {
     }
 
     for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n - i + 1; j++)
+      for (int j = 0; j < n - i; j++)
         System.out.print(arr[i][j] + "\t");
       System.out.println();
     }
